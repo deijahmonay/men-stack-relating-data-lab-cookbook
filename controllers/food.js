7fff809c4a28b1498ad6df6908b46be767ec2f6c
deijahmonay <deijah@shack15.com>
2024-10-10 21:47:37 -0700

    create index route for landing page

--- controllers/food.js
+++ controllers/food.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 
+router.get('/', (req, res) => {
+  res.render('foods/index.ejs')
+});
 
 
 module.exports = router;
