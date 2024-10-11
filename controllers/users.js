802851098d9c0ae88ec0837f5f593966e274d292
deijahmonay <deijah@shack15.com>
2024-10-11 09:55:24 -0700

    add note for code signin user

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user.js');
+const User = require('../models/user.js'); 
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res) => { 
   const allUsers = await User.find({})
   res.render('users/index.ejs', {
       allUsers
   })
 })
 
-router.get('/:userId', async (req, res) => {
-  const user = await User.findById(req.params.userId)
-  res.render('users/show.ejs', {
-      user
+router.get('/:userId', async (req, res) => { // display one user by id
+  const user = await User.findById(req.params.userId) // find user by id
+  res.render('users/show.ejs', { //render user/show.ejs, pass to template
+      user // send user ti the view
   })
 })
 module.exports = router;
