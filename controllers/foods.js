66fc149c84de7a7a0630dd9290a89a710140a3dd
deijahmonay <deijah@shack15.com>
2024-10-11 09:47:37 -0700

    add notes detailing each route

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,69 +3,64 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', async (req, res) => {
-try {
-const currentUser = await User.findById(req.session.user._id)
-res.render('foods/index.ejs', {
-pantry: currentUser.pantry
+router.get('/', async (req, res) => { // MAIN FOOD PAGE ROUTE 
+try { 
+const currentUser = await User.findById(req.session.user._id) //GET CURRENT USERS DATA AND KEEP BY SESSION
+res.render('foods/index.ejs', { // render food/index.ejs, dispaly users pantry data
+pantry: currentUser.pantry // DISPALY PANTRY ITEMS
     })
-} catch(error) {
-    console.log(error)
+} catch(error) { // redirect to homepage if error happens
     res.redirect('/')
 }
 })
 
-router.get('/new', (req, res) => {
-res.render('foods/new.ejs')
+router.get('/new', (req, res) => {  // route to show new form for new food items
+res.render('foods/new.ejs') // render to food/new.ejs to show the new form
 })
 
-router.get('/:foodsId/edit', async (req, res) => { 
+router.get('/:foodsId/edit', async (req, res) => {  //display edit form route for selevcted food item
 try {
-    const currentUser = await User.findById(req.session.user._id) 
-    const editFood = currentUser.pantry.id(req.params.foodsId)
-    console.log(editFood)
-    res.render('foods/edit.ejs', {
-        pantry: editFood
+    const currentUser = await User.findById(req.session.user._id) // find the current user in the database by session id
+    const editFood = currentUser.pantry.id(req.params.foodsId) //edit food item by food id 
+    res.render('foods/edit.ejs', { // render foodejs page (specific food item)
+        pantry: editFood // moving the food item to the edut form
     })
-} catch (error) {
-    console.log(error)
+} catch (error) { // if an error appears redirect to homepage
     res.redirect('/')
 }
 })
 
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res) => { // route for processing the newly created food item
 try {
-    const currentUser = await User.findById(req.session.user._id) 
-    currentUser.pantry.push(req.body) 
-    await currentUser.save() 
-    res.redirect(`/users/${currentUser._id}/foods`) 
+    const currentUser = await User.findById(req.session.user._id)  // find user in database
+    currentUser.pantry.push(req.body) // add new food item to users pantry
+    await currentUser.save()  // save the users to databae once process complete
+    res.redirect(`/users/${currentUser._id}/foods`)  // redirect user to updated list of foods
 } catch (error){
-    console.log(error)
-    res.redirect('/') 
+    res.redirect('/')  // if an error appears redirect to homepage
 }
 })
 
-router.put('/:foodsId', async (req, res) => {
+router.put('/:foodsId', async (req, res) => { // route for updating food item that already exists
 try {
-    const currentUser = await User.findById(req.session.user._id)
-    const updateFood = currentUser.pantry.id(req.params.foodsId) // does foodsId need to be pantryId?
-    updateFood.set(req.body) 
-    await currentUser.save()
-    res.redirect(`/users/${currentUser._id}/foods`)
-} catch (error) {
+    const currentUser = await User.findById(req.session.user._id) //same as above, find user in database by id
+    const updateFood = currentUser.pantry.id(req.params.foodsId)  // find specific food item in pantry by id
+    updateFood.set(req.body)  // update food item withh new data
+    await currentUser.save() // save updated info to user database
+    res.redirect(`/users/${currentUser._id}/foods`) // redirect to foods lists 
+} catch (error) { // if an error occures redirect to homepage
     res.redirect('/')
 }
 })
 
-router.delete('/:foodsId', async (req, res) => {
+router.delete('/:foodsId', async (req, res) => { // route to delete food item
 try {
-    const currentUser = await User.findById(req.session.user._id)
-    currentUser.pantry.id(req.params.foodsId).deleteOne()
-    await currentUser.save()
-    res.redirect(`/users/${currentUser._id}/foods`)
-} catch (error) {
-    console.log(error)
+    const currentUser = await User.findById(req.session.user._id) //same as above, find user in database by id
+    currentUser.pantry.id(req.params.foodsId).deleteOne() // find item in user pantry by id and delete that specific item
+    await currentUser.save() // once complete save to database
+    res.redirect(`/users/${currentUser._id}/foods`) // redirect to lists of all food items
+} catch (error) { // if an error occures redirect to homepage
     res.redirect('/')
 }
 })
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,6 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
   const allUsers = await User.find({})
-  console.log(allUsers)
   res.render('users/index.ejs', {
       allUsers
   })
